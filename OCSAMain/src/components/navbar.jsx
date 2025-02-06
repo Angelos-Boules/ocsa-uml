7fe23ed7281a9de6626137fb8ad6428d0fa8a1c8
Angelos Boules <angelos.boules24@gmail.com>
2025-02-06 15:04:37 -0500

    Added and updated git ignore files

--- OCSAMain/src/components/navbar.jsx
+++ OCSAMain/src/components/navbar.jsx
@@ -7,19 +7,19 @@ function Navbar() {
     
     return (     
         <header>                   
-        <div className= "navbar bg-dark text-light">
-            {/* Heading - diff screen sizes */}
-            <header className="fs-1 banner mx-auto d-none d-md-block"> Orthodox Christian Student Association </header>
-            <header className="brand navbar-brand fs-1 banner d-md-none"> 
-                <img src="../../orthocross.svg" height="35px" alt="Orthodox Cross Icon"/>
-                O.C.S.A. 
-            </header>
-            
-            {/* Hamburger Menu Button + Icon */}
-            <button className="drawer-button-custom d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <img src="../../menu-icon.png" height="35px"/>
-            </button>
-        </div>
+            <div className= "navbar bg-dark text-light">
+                {/* Heading - diff screen sizes */}
+                <header className="fs-1 banner mx-auto d-none d-md-block"> Orthodox Christian Student Association </header>
+                <header className="brand navbar-brand fs-1 banner d-md-none"> 
+                    <img src="../../orthocross.svg" height="35px" alt="Orthodox Cross Icon"/>
+                    O.C.S.A. 
+                </header>
+                
+                {/* Hamburger Menu Button + Icon */}
+                <button className="drawer-button-custom d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <img src="../../menu-icon.png" height="35px"/>
+                </button>
+            </div>
 
             {/* Nav Menu Items
             <nav className="navbar navbar-dark bg-dark">

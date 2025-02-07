dd9f34a71f989f19b3677fdd16978e3fa189f5b9
Angelos Boules <angelos.boules24@gmail.com>
2025-02-06 22:44:39 -0500

    Finish <Navbar /> component including all nav menu responsiveness

--- OCSAMain/src/App.jsx
+++ OCSAMain/src/App.jsx
@@ -1,5 +1,6 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
+
 import "./main.css"
 import React from "react";
 import Navbar from "./components/navbar";
@@ -8,7 +9,8 @@ import Carousel from "./components/carousel";
 function App() {
     return (
         <div>
-            <Navbar />
+        <Navbar />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus cupiditate eligendi debitis commodi eum expedita, natus rem molestias reprehenderit officiis quod enim qui assumenda laborum! Veniam sit veritatis, placeat ad, illo adipisci quam rerum voluptatum ratione, aut temporibus quia. Unde quidem laboriosam voluptatibus exercitationem, sapiente placeat sed error itaque veritatis! Libero, natus. Repellendus, beatae, expedita sequi obcaecati vitae nulla est neque ut quae iure inventore eaque corporis rerum blanditiis error qui? Beatae repellat eum corporis aperiam velit corrupti, eveniet officia repellendus, vero voluptatem vel reprehenderit, ab maxime? Illo facilis sit quaerat nostrum repellendus vel, error quo quas, maiores doloremque autem.
         </div>
     )
 }
--- OCSAMain/src/components/navbar.jsx
+++ OCSAMain/src/components/navbar.jsx
@@ -1,31 +1,83 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+    const [isFixed, setIsFixed] = useState(false);
 
     const toggleMenu = () => setIsOpen(!isOpen);
     
+    // Snap navbar to fixed top once it reaches the top by scrolling
+    const handleScroll = () => {
+        const navbar = document.querySelector('.nav-wrapper-custom');
+
+        if (window.scrollY > navbar.getBoundingClientRect().height) {
+            setIsFixed(true);
+        } else {
+            setIsFixed(false);
+        }
+    };
+
+    // Add event listener to the scroll when re-rendering and call handle scroll
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, []);
+
+    // Close drawer menu by clicking outside of it
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (isOpen) {
+                const sidePane = document.querySelector(".side-pane");
+                if (sidePane && !sidePane.contains(event.target)) {
+                    setIsOpen(false);
+                }
+            }
+        };
+    
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isOpen]);
+
     return (     
-        <header>                   
-            <div className= "navbar bg-dark text-light">
-                {/* Heading - diff screen sizes */}
-                <header className="fs-1 banner mx-auto d-none d-md-block"> Orthodox Christian Student Association </header>
-                <header className="brand navbar-brand fs-1 banner d-md-none"> 
+        <header className="nav-object-wrapper">                   
+            <div className= "navbar bg-dark text-light div-wrapper-custom">
+                
+                {/* Heading - medium + large screens */}
+                <header className="fs-1 banner mx-auto d-none d-md-block"> 
+                    Orthodox Christian 
                     <img src="../../orthocross.svg" height="35px" alt="Orthodox Cross Icon"/>
-                    O.C.S.A. 
+                    Student Association 
                 </header>
                 
-                {/* Hamburger Menu Button + Icon */}
-                <button className="drawer-button-custom d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                    <img src="../../menu-icon.png" height="35px"/>
-                </button>
+                {/* Wrapper div for fixed smaller menu nav */}
+                <div className="navbar-fixed d-md-none">
+                    <div className="navbar bg-dark text-light div-wrapper-custom"> 
+                        {/* Heading - small screens */}
+                        <header className="brand navbar-brand fs-1 banner d-md-none"> 
+                            <img src="../../orthocross.svg" height="35px" alt="Orthodox Cross Icon"/>
+                            O.C.S.A. 
+                        </header>
+                        
+                        {/* Hamburger Menu Button + Icon */}
+                        <button className="drawer-button-custom d-md-none" 
+                            type="button" onClick={toggleMenu}>
+                            <img src="../../menu-icon.png" height="35px"/>
+                        </button>
+                    </div>
+                </div>
+            
             </div>
 
-            {/* Nav Menu Items
-            <nav className="navbar navbar-dark bg-dark">
-            <div className="container">                    
-                <ul className="navbar-nav mx-auto d-flex flex-row gap-3">
-                                        <li className="nav-item">
+            {/* Navbar for larger screen sizes */}
+            <nav className={`navbar navbar-dark bg-dark d-none d-md-block nav-wrapper-custom
+                ${isFixed ? 'fixed-top' : ''}`} id="menubar-custom">
+                <div className="container">
+                    <ul className="navbar-nav mx-auto d-flex flex-row gap-3">
+                        <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
@@ -39,15 +91,29 @@ function Navbar() {
                         </li>
                     </ul>
                 </div>
-            </nav> */}
+            </nav>
+
+            {/* Navbar side pane for smaller screens */}
+            <div className={`side-pane ${isOpen ? 'open' : ''} d-md-none`}>
+                <nav className="navbar navbar-dark bg-dark">
+                    <ul className="navbar-nav mx-auto d-flex flex-column text-center gap-3">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Shop</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Events</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
         </header>
-
-
-
-
-
-
-
     );
 }
 

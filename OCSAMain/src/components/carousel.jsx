5f25b3a9649a7ffcbac92b52aa7199bafd363cc0
Angelos Boules <angelos.boules24@gmail.com>
2025-02-07 13:15:38 -0500

    Finished carousel design. TODO: Implement carousel functionality

--- OCSAMain/src/components/carousel.jsx
+++ OCSAMain/src/components/carousel.jsx
@@ -1,33 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import carouselItems from "../assets/carouselItems";
 
-function Carousel() {
-    const [currIndex, setCurrIndex] = useState(0);
+function Carousel(props) {
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    const nextItem = () => {
-        setCurrIndex(prevIndex => (prevIndex + 1) % carouselItems.length);
+    const getNextItem = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % carouselItems.length);
     };
 
-    const prevItem = () => {
-        setCurrIndex(prevIndex => (prevIndex - 1 + carouselItems.length) % carouselItems.length);
+    const getLastItem = () => {
+        setCurrentIndex((prevIndex) => prevIndex === 0 ? carouselItems.length - 1 : prevIndex + 1);
     };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            getNextItem();
+        },3000);
+
+        return () => clearInterval(interval)
+    }, [currentIndex]); {/* Reset interval count everytime the current one changes */}
 
     return (
-        <div className="carousel-container">
-            {/* Carousel item */}
-            test
-            <div className="carousel-item">
-                <img src={carouselItems[currIndex].image}/>
-                {console.log(carouselItems[currIndex].title)}
-                <h2>{carouselItems[currIndex].title}</h2>
-                <p>{carouselItems[currIndex].desc}</p>
-            </div>
+        <div className="carousel-top-level">
+            <div className="carousel-container w-75">
+                <img src="https://placehold.co/600x400"/>
+                <h1 className="carousel-header"> Hello World </h1>
+                <p className="carousel-description"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat illo mollitia consequatur inventore hic exercitationem eos culpa ut possimus debitis.</p>
+                <button className="carousel-button next-button" onClick={getNextItem}>
+                    <i class="bi bi-caret-right-fill"></i>
+                </button>
+                <button className="carousel-button last-button" onClick={getLastItem}>
+                    <i class="bi bi-caret-left-fill"></i>
+                </button>
 
-            {/* Navigation buttons */}
-            <button onClick={prevItem} className="prev-button">Prev</button>
-            <button onClick={nextItem} className="next-button">Next</button>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Carousel;

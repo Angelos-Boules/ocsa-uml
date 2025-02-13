1013adf14ee59ecfbb9c436e12bd74ddc4ce8e59
Angelos Boules <angelos.boules24@gmail.com>
2025-02-13 12:54:08 -0500

    Added status buttons to carousel

--- OCSAMain/src/assets/carouselItems.js
+++ OCSAMain/src/assets/carouselItems.js
@@ -13,7 +13,23 @@ const carouselItems = [
         title: "EVENT ALERT!",
         image: "https://placehold.co/090x100",
         desc: "OCSA is hitting the slopes! Join us as we ski Pat's Peak on 2/15. Sign up by this saturday"
-    }
+    },    {
+        title: "Annual OCSA Election",
+        image: "https://placehold.co/400x600",
+        desc: "Here are your NEW elected E-board members for the upcoming year!"
+    },
+    {
+        title: "New Sweatshirt Design!",
+        image: "https://placehold.co/200x300",
+        desc: "Our new merch designs have dropped! Make sure to place your orders by next week."
+    },
+    {
+        title: "EVENT ALERT!",
+        image: "https://placehold.co/090x100",
+        desc: "OCSA is hitting the slopes! Join us as we ski Pat's Peak on 2/15. Sign up by this saturday"
+    },
+
+
 ]
 
 export default carouselItems;
--- OCSAMain/src/components/carousel.jsx
+++ OCSAMain/src/components/carousel.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function Carousel({ items }) {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [paused, setPaused] = useState(false);
 
     const getNextItem = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
@@ -15,13 +16,19 @@ function Carousel({ items }) {
         setCurrentIndex(index);
     };
 
+    const togglePause = () => {
+        setPaused((prevPaused) => !prevPaused);
+    }
+
     useEffect(() => {
+        if (paused) return;
+
         const interval = setInterval(() => {
             getNextItem();
         }, 4000); 
 
         return () => clearInterval(interval)
-    }, [currentIndex]);
+    }, [currentIndex, paused]);
 
     return (
         <div className="carousel-top-level">
@@ -35,6 +42,19 @@ function Carousel({ items }) {
                 <button className="carousel-button last-button" onClick={getLastItem}>
                     <i class="bi bi-caret-left-fill"></i>
                 </button>
+
+                {/* Status Buttons */}
+                <div className="carousel-controls">
+                    <div className="carousel-status-buttons">
+                        {items.map((_, index) => (
+                            <button 
+                                key = {index}
+                                className = {`status-button ${index === currentIndex ? 'active' : ''}`}
+                                onClick = {() => handleCarouselButtonClick(index)}
+                            ></button>
+                        ))}
+                    </div>
+                </div>
 
             </div>
         </div>
